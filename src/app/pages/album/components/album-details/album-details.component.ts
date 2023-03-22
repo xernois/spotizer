f31ad8330cfa6bb22deeb38575d15f6bcb47a0ad
Killian <gren.kiki@gmail.com>
2023-03-22 21:54:26 +0100

    album details css and structure

--- src/app/pages/album/components/album-details/album-details.component.ts
+++ src/app/pages/album/components/album-details/album-details.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { PlayerService } from '@src/app/services/player.service';
 import { Album } from '@src/app/models/album.model';
+import { Song } from '@src/app/models/song.model'
+import { Artist } from '@src/app/models/artist.model';
 
 @Component({
   selector: 'app-album-details',
@@ -10,15 +13,24 @@ import { Album } from '@src/app/models/album.model';
 export class AlbumDetailsComponent {
 
   album !: Album
+  artist !: Artist
 
   constructor(
     private route: ActivatedRoute,
+    private playerService: PlayerService,
     private router : Router
   ) {}
 
   async ngOnInit() {
     this.album = this.route.snapshot.data['album'][0]
-
+    this.album.getArtist().subscribe((artist) => this.artist = artist)
     if(!this.album) this.router.navigateByUrl('/album')
+  }
+
+  playSong(id: number) {
+    this.playerService.musicQueue = [...this.album.songs]
+    for (let i = 0; i< id; i++) {
+      this.playerService.nextSong()
+    }
   }
 }
