f8f2d9688e0f870d2e95f781edeb34c13e577b10
xernois <antoine2496@gmail.com>
2023-04-07 14:42:12 +0200

    song length + addQueue

--- src/app/components/floating-menu/floating-menu.component.ts
+++ src/app/components/floating-menu/floating-menu.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Playlist } from '@src/app/models/playlist.model';
 import { Song } from '@src/app/models/song.model';
 import { PlaylistService } from '@src/app/services/api/playlist.service';
+import { PlayerService } from '@src/app/services/player.service';
 
 @Component({
   selector: 'app-floating-menu',
@@ -19,11 +20,16 @@ export class FloatingMenuComponent {
   public expanded: boolean = false;
 
   constructor(
+    private playerService: PlayerService,
     private playlistService: PlaylistService
   ) { }
 
   ngOnInit() {
     this.playlistService.getPlaylists().subscribe(playlists => this.playlists = playlists)
+  }
+
+  addQueue() {
+    this.playerService.musicQueue.push(this.song)
   }
 
   addToPlaylist(playlistId: number) {
